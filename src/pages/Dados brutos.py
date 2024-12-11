70bdf47e1e1e1fd336ab1c51f57478610835ec26
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2024-12-10 21:30:31 -0300

    feat: adiciona filtros e download

--- src/pages/Dados brutos.py
+++ src/pages/Dados brutos.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import requests 
 import pandas as pd
+import time
+
+@st.cache_data
+def convert_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso', icon='✅')
+    time.sleep(5)
+    sucesso.empty()
+    
 
 st.title('Dados Brutos')
 url = 'https://labdados.com/produtos'
@@ -8,6 +19,8 @@ url = 'https://labdados.com/produtos'
 response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format= '%d/%m/%Y')
+
+
 
 with st.expander('Colunas'):
     colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
@@ -25,14 +38,40 @@ with st.sidebar.expander('Preço do produto'):
 with st.sidebar.expander('Data da compra'):
     data_compra = st.date_input('Seleciona a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
 with st.sidebar.expander('Vendedor'):
-    st.multiselect('Selecione os vendedores', dados['Vendedor'].unique(), dados['Vendedor'].unique())
+   vendedores = st.multiselect('Selecione os vendedores', dados['Vendedor'].unique(), dados['Vendedor'].unique())
 with st.sidebar.expander('Local da compra'):
-    st.multiselect('Selecione o local da compra', dados['Local da compra'].unique(), dados['Local da compra'].unique())
+    local_compra = st.multiselect('Selecione o local da compra', dados['Local da compra'].unique(), dados['Local da compra'].unique())
 with st.sidebar.expander('Avaliação da compra'):
-    st.slider('Selecione a avaliação da compra', 1,5, value=(1,5))
+    avaliacao = st.slider('Selecione a avaliação da compra', 1,5, value=(1,5))
 with st.sidebar.expander('Tipo de pagamento'):
     tipo_pagamento = st.multiselect('Selecione o tipo de pagamento',dados['Tipo de pagamento'].unique(), dados['Tipo de pagamento'].unique())
 with st.sidebar.expander('Quantidade de parcelas'):
     qtd_parcelas = st.slider('Selecione a quantidade de parcelas', 1, 24, (1,24))
     
-st.dataframe(dados)
+    
+query = '''
+Produto in @produtos and \
+`Categoria do Produto` in @categoria_produtos and \
+@preco[0] <= Preço <= @preco[1] and \
+@frete[0] <= Frete <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+`Local da compra` in @local_compra and \
+@avaliacao[0]<= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
+'''
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+    
+st.dataframe(dados_filtrados)
+st.markdown(f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas :blue[{dados_filtrados.shape[1]}] colunas')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+
+with coluna1:
+    nome_arquivo = st.text_input('', label_visibility='collapsed', value='dados')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer o download da tabela em csv', data=convert_csv(dados_filtrados), file_name=nome_arquivo, mime='text/csv', on_click=mensagem_sucesso())
